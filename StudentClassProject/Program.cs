71c630790dfa43c95807cb2847c79896199231c7
Lidya Tsegay <lidyatsegay@outlook.com>
2019-08-27 16:21:04 -0400

    Add project files.

---
+++ StudentClassProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StudentClassProject {
+    class Program {
+        static void Main(string[] args) {
+            var Lidya = new Student();
+            Lidya.Firstname = "Lidya";
+            Lidya.Lasttname = "Tsegay";
+            Lidya.SetFinalGrade(98);
+            var fullname = Lidya.GetFullname();
+            Console.WriteLine("Lidya's full name is " + fullname);
+
+            var Justin = new Student();
+            Justin.Firstname = "Justin";
+            Justin.Lasttname = "Dudley";
+            Justin.SetFinalGrade(99);
+            fullname = Justin.GetFullname();
+            Console.WriteLine($"{fullname}");
+
+            var Nick = new Student();
+            Nick.Firstname = "Nick";
+            Nick.Lasttname = "Baker";
+            Nick.SetFinalGrade(97);
+            fullname = Nick.GetFullname();
+            Console.WriteLine($"{Nick.Firstname} {Nick.Lasttname}");
+            
+
+        }
+    }
+}
---
+++ StudentClassProject/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentClassProject {
+
+    class Student { //Class and Name
+
+        public string Firstname { get; set; } //Properties
+        public string Lasttname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; } // "public" is an access modifier
+        public int FinalGrade { get; set; }
+
+        public string GetFullname() {
+            return Firstname + " " + Lasttname;
+        }
+
+        public void SetFinalGrade(int finalgrade) { //if method doesn't return anything, make sure you have void for type
+            if (finalgrade < 0) {
+                Console.WriteLine("ERROR: grade must be greater than zero");
+            }
+            else {
+                FinalGrade = finalgrade;
+            }
+        }
+
+
+    }
+}
